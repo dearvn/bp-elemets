0d62fda0dbc2c398c4b89a7214b6a70f24e26e0f
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2022-06-22 09:42:19 +0700

    init

---
+++ bp.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  BestProduct Elements
+Plugin URI:   https://bestproducts.com.vn 
+Description:  A Library That Lets You Reimagine the Way You Use the WordPress Block Editor.
+Version:      1.0
+Author:       Donald 
+Author URI:   https://bestproducts.com.vn 
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
